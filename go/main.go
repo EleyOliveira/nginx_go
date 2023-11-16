66dc825c354e60892de00e8205a706f8307c2737
EleyOliveira <eleyoliveira@bol.com.br>
2023-11-16 20:22:13 -0300

    ajuste na estrutura do projeto e adicionado gitignore

--- go/main.go
+++ go/main.go
@@ -1,3 +1,4 @@
+//package = compila como um executável
 package main
 
 import (
@@ -6,14 +7,17 @@ import (
 )
 
 func main() {
+	//Cria a primeira rota
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Olá Osasco")
 	})
 
+	//Cria uma rota que recebe um parâmetro URL
 	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
 		name := r.URL.Path[len("/greet/"):]
 		fmt.Fprintf(w, "Olá %s\n", name) 
 	})
 
+	//Instancia o servidor
 	http.ListenAndServe(":9990", nil)
 }
